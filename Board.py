7fabbe2406545b2bcde23a391eeec7c4ecf7fb40
martingundhus <martin.gundhus.1072@student.uu.se>
2024-09-16 14:33:31 +0200

    added board and player classes

---
+++ Board.py
@@ -0,0 +1,16 @@
+class Board:
+    def __init__(self, board_size, grid):
+        self.board_size = 5
+        self.grid = grid
+
+    def createBoard(self):
+        for x in range(self.board_size):
+            for y in range(self.board_size):
+                self.grid[x][y] = CreateStack() #Stack group write this!
+
+    def placeStone(self, x, y):
+        self.grid[x][y] = addStone() #Stack group write this!
+
+    def moveStone(self, xFrom, yFrom, xTo, yTo):
+        self.grid[xFrom][yFrom] = removeStone() #Stack group write this!
+        self.grid[xTo][yTo] = addStone()
