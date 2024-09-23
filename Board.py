d51dd05a80e0fee57adbbfd82442f519db1ec080
Alicebernt <alice.berntsson.3891@student.uu.se>
2024-09-23 16:09:48 +0200

    test files and added fucntions to board.pyboard

--- Board.py
+++ Board.py
@@ -1,58 +1,80 @@
-import game
+import Game
 import stone
 import stack
-import player
+import Player
+
+import numpy as np
+
 
 class Board:
-    def __init__(self, board_size, grid):
-        self.board_size = 5
-        self.grid = grid
+    def __init__(self, board_size):
+        self.board_size = board_size
+        self.grid = np.empty([self.board_size, self.board_size],dtype = stack.Stack)
+        self.picked_up_stack = stack.Stack()
+        self.turn = 0
+        self.current_x = -1
+        self.current_y = -1
+        for r in range(board_size):
+            for c in range(board_size):
+                self.grid[r][c] = stack.Stack()
+                
+    # def createBoard(self):
+    #     for x in range(self.board_size):
+    #         for y in range(self.board_size):
+    #             self.grid[x][y] = stack.Stack() #Stack group write this!
 
-    def createBoard(self):
-        for x in range(self.board_size):
-            for y in range(self.board_size):
-                self.grid[x][y] = stack.Stack() #Stack group write this!
+    def changeTurn(self):
+        self.picked_up_stack = stack.Stack()
+        if self.turn == 0:
+            self.turn = 1
+        else:
+            self.turn = 0
 
     def getStack(self, x, y):
         return self.grid[x][y]
 
     def placeStone(self, x, y, upright_input, player_index):
-        self.grid[x][y] = stack.Stack.push_stone(player_index, upright_input)  #Stack group write this!
+        if self.getStack(x,y).stackable:
+            self.getStack(x,y).push_stone(player_index, upright_input)  #Stack group write this!
+            self.changeTurn()
+            Player.Player.useStone()
+        else:
+            raise TypeError("Not valid move")
 
     def checkLeft(self, x, y):
-        if (x != 0 and self.grid[x-1][y].stack.Stack.stackable):
+        if (x != 0 and self.getStack(x-1,y).stackable):
             return True
         else:
             return False
             
     def checkRight(self, x, y):
-        if ( x != 4 and self.grid[x+1][y].stack.Stack.stackable):
+        if ( x != 4 and self.getStack(x+1,y).stackable):
             return True
         else:
             return False
 
     def checkDown(self, x, y):
-        if (y != 0 and self.grid[x][y-1].stack.Stack.stackable):
+        if (y != 0 and self.getStack(x, y-1).stackable):
             return True
         else:
             return False
 
     def checkUp(self, x, y):
-        if (y != 4 and self.grid[x][y+1].stack.Stack.stackable):
+        if (y != 4 and self.getStack(x,y+1).stackable):
             return True
         else:
             return False
 
 
-    def enoughStones(tileStack):
-        if (stack.Stack.height(tileStack) >= 2):  # Stack Group have written this!
-            return True
-        else:
-            return False
+    # def enoughStones(tileStack):
+    #     if (stack.Stack.height(tileStack) >= 2):  # Stack Group have written this!
+    #         return True
+    #     else:
+    #         return False
         
 
-    def isValidMove(self, playerIndex, xFrom, xTo, yFrom, yTo):
-        if (self.enoughStones(Board.getStack(xFrom, yFrom)) and check_top_stone(playerIndex)):
+    def isValidMove(self, xFrom, xTo, yFrom, yTo):
+        if (self.getStack(yFrom, yTo).stackable):
             if (xFrom > xTo):
                 if (self.checkLeft(xFrom, yFrom)):
                      return True
@@ -66,8 +88,23 @@ class Board:
                 if (self.checkUp(xFrom, yFrom)):
                      return True
 
+    def pickUpStack(self, x, y):
+        if self.getStack(x,y).height() >= 1 and self.getStack(x,y).check_top_stone(self.turn):
+            self.picked_up_stack = self.getStack(x,y)
+            self.grid[x][y] = stack.Stack()
+            self.current_x = x
+            self.current_y = y
+        else:
+            raise TypeError("Cannot pick up this stack")
+
     def moveStack(self, xFrom, yFrom, xTo, yTo):
         if (self.isValidMove(xFrom, yFrom, xTo, yTo)):
-            moveAndLeaveBottomStone() #HOW???
+            self.picked_up_stack.drop_stone(self.getStack(xTo, yTo)) #HOW???
+            self.current_x = xTo
+            self.current_y = yTo
+            if self.picked_up_stack.height() == 0:
+                self.changeTurn()
         else:
             raise TypeError("Not valid move")
+        
+    
--- test_board.py
+++ test_board.py
@@ -3,10 +3,131 @@ from collections import deque
 import stone
 import stack
 import Board
+import numpy as np
 
 class TestBoard(unittest.TestCase):
     def setUp(self):
-        pass
+        self.board = Board.Board(board_size=5)
 
+    def test_stack_on_tile(self):
+        self.assertEqual(self.board.getStack(0,0).stack_content, deque(), "Should be an empty stack here")
+        self.assertEqual(self.board.getStack(0,1).stack_content, deque(), "Should be an empty stack here")
+        self.assertEqual(self.board.getStack(4,3).stack_content, deque(), "Should be an empty stack here")
+
+        self.assertTrue(self.board.getStack(0,0).stackable, "Should be stackable")
+        self.assertTrue(self.board.getStack(0,1).stackable, "Should be stackable")
+        self.assertTrue(self.board.getStack(4,3).stackable, "Should be stackable")
+        
+    def test_place_stone(self):
+        self.board.placeStone(0,4,False,1)
+        self.assertFalse(self.board.getStack(0,4).stack_content[0].upright)
+        self.assertEqual(self.board.getStack(0,4).stack_content[0].player_index, 1)
+        self.board.placeStone(2,4,True,0)
+        self.assertTrue(self.board.getStack(2,4).stack_content[0].upright)
+        self.assertEqual(self.board.getStack(2,4).stack_content[0].player_index, 0)
+       
+    def check_stack_height_and_stackableness(self):
+        self.board.placeStone(0,0,False,1)
+        self.assertTrue(self.board.getStack(0,0).stackable, "Should be stackable")
+        self.assertEqual(self.board.getStack(0,0).height(), 1, "Should be height 1")
+        self.board.placeStone(0,0,True,1)
+        self.assertFalse(self.board.getStack(0,0).stackable, "Shouldn't be stackable")
+        self.assertEqual(self.board.getStack(0,0).height(), 2, "Should be height 2")
+  
+    def check_direction_test_false(self):
+        self.board.placeStone(3,4,True,1)
+        self.board.placeStone(3,2,True,1)
+        self.board.placeStone(2,3,True,1)
+        self.board.placeStone(4,3,True,1)
+        self.assertFalse(self.board.checkRight(3,3), "Shouldn't be able to move right")
+        self.assertFalse(self.board.checkLeft(3,3), "Shouldn't be able to move left")
+        self.assertFalse(self.board.checkUp(3,3), "Shouldn't be able to move up")
+        self.assertFalse(self.board.checkDown(3,3), "Shouldn't be able to move down")
+
+    def check_direction_test_true(self):
+        self.board.placeStone(3,4,False,1)
+        self.board.placeStone(3,2,False,1)
+        self.board.placeStone(2,3,False,1)
+        self.board.placeStone(4,3,False,1)
+        self.assertTrue(self.board.checkRight(3,3), "Should be able to move right")
+        self.assertTrue(self.board.checkLeft(3,3), "Should be able to move left")
+        self.assertTrue(self.board.checkUp(3,3), "Should be able to move up")
+        self.assertTrue(self.board.checkDown(3,3), "Should be able to move down")
+
+        #checking empty tile then adding a stone and checking again
+        self.assertTrue(self.board.checkUp(1,2), "Should be able to move up")
+        self.board.placeStone(1,3,True,1)
+        self.assertFalse(self.board.checkUp(1,2), "Shouldn't be able to move up")
+
+    def pick_up_stack(self):
+        self.board.placeStone(3,4,False,1)
+        self.board.placeStone(3,4,False,1)
+        self.board.placeStone(3,4,False,1)
+        self.board.pickUpStack(3,4)
+        self.assertEqual(self.board.current_x, 3)
+        self.assertEqual(self.board.current_y, 4)
+        right_answer = stack.Stack()
+        right_answer.push_stone(1, False)
+        right_answer.push_stone(1, False)
+        right_answer.push_stone(1, False)
+        self.assertEqual(self.board.picked_up_stack.stack_content, right_answer)
+
+    def test_move_stack(self):
+        self.board.current_x = 3
+        self.board.current_y = 3
+        self.board.placeStone(self.board.current_x, self.board.current_y, False, 1)
+        self.board.placeStone(3,4,False,1)
+        self.board.placeStone(3,4,False,1)
+        self.board.pickUpStack(3,3)
+        self.board.moveStack(self.board.current_x, self.board.current_y, 3, 4)
+        self.assertEqual(self.board.getStack(3,4).height(), 3, "Should be height 3")
+        self.assertEqual(self.board.getStack(3,3).height(), 0, "Should be height 0")
+        self.assertEqual(self.board.current_x, 3)
+        self.assertEqual(self.board.current_y, 4)
+        
+        with self.assertRaises(TypeError):
+            self.board.placeStone(3, 3, True, 1)
+            self.board.pickUpStack(3, 4)
+            self.board.moveStack(self.board.current_x, self.board.current_y, 3, 3)
+
+        
+    def test_move_stack_far(self):
+        self.board.current_x = 0
+        self.board.current_y = 1
+        self.board.turn = 1
+        self.board.placeStone(0,1,False,1)
+        self.board.placeStone(0,1,False,1)
+        self.board.placeStone(0,1,False,1)
+        self.board.placeStone(0,1,False,1)
+        self.board.placeStone(0,1,False,1)
+        self.board.placeStone(0,1,False,1)
+        self.board.placeStone(0,1,False,1)
+        self.board.placeStone(0,1,True,1)
+
+        self.board.placeStone(2,1,False,1)
+        self.board.placeStone(2,1,False,0)
+        self.board.placeStone(2,1,False,1)
+        self.board.placeStone(2,1,False,0)
+
+        self.board.pickUpStack(0,1)
+        self.board.moveStack(self.board.current_x, self.board.current_y, 0, 1)
+        self.board.moveStack(self.board.current_x, self.board.current_y, 0, 1)
+        self.board.moveStack(self.board.current_x, self.board.current_y, 1, 1)
+        self.board.moveStack(self.board.current_x, self.board.current_y, 2, 1)
+        self.board.moveStack(self.board.current_x, self.board.current_y, 3, 1)
+        self.board.moveStack(self.board.current_x, self.board.current_y, 3, 1)
+        self.board.moveStack(self.board.current_x, self.board.current_y, 3, 1)
+        self.board.moveStack(self.board.current_x, self.board.current_y, 4, 1)
+
+        self.assertEqual(self.board.getStack(0,1).height(), 2, "Should be height 2")
+        self.assertEqual(self.board.getStack(1,1).height(), 1, "Should be height 1")
+        self.assertEqual(self.board.getStack(2,1).height(), 5, "Should be height 5")
+        self.assertEqual(self.board.getStack(3,1).height(), 3, "Should be height 3")
+        self.assertEqual(self.board.getStack(4,1).height(), 1, "Should be height 1")
+        self.assertEqual(self.board.picked_up_stack.height(), 0, "Should be height 0")
+
+        self.assertEqual(self.board.turn, 0, "Should be turn 0")
+
+        
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main() 
